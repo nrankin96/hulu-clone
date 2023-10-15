d8588e05c069658b5aa08f73bbfbb749ec5d2c15
Nykia <nykiarankin96@gmail.com>
2023-10-15 13:15:29 -0400

    finish hero

--- app/page.js
+++ app/page.js
@@ -1,30 +1,51 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import Navbar from '@/components/Navbar';
-
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className='bg-hero bg-cover bg-center w-full h-[800px] '>
+    <main className="bg-hero bg-cover bg-center w-full h-[700px] md:h-[600px]">
       <Navbar />
-      <div className='flex flex-row justify-around items-center flex-wrap my-[140px] text-[#00ED82] uppercase text-[13px] font-bold  '>
-        <div className='flex flex-col items-center'>
-            <h3 className='mb-4'>Disney Bundle Duo Basic</h3>
-            <Image src='/logo-bundleDis.png' alt='logo-bundle'width={150} height={200}/>
-            <o className='mt-4 ml-4 text-white text-[16px] text-center font-semibold'>Both with ads, for <br />  $9.99/month.</o>
-            <button className='text-black mt-4 bg-[#00ED82] w-full h-[40px] rounded-sm uppercase'>Get Them Both</button>
+      <div className="flex flex-row space-x-10 justify-center md:mx-20 items-center flex-wrap mt-[140px] md:mt-[80px] text-[#00ED82] text-[13px] font-bold  ">
+        <div className="flex flex-col justify-center items-center md:max-w-64 lg:w-[300px] lg:h-[300px]">
+          <h3 className="mb-4">Disney Bundle Duo Basic</h3>
+          <Image
+            src="/logo-bundleDis.png"
+            alt="logo-bundle"
+            width={150}
+            height={200}
+          />
+          <o className="mt-4 ml-4 text-white text-[16px] md:text-[25px] text-center font-normal">
+            Both with ads, for <br /> $9.99/month.
+          </o>
+          <button className="text-black mt-4 bg-[#00ED82] w-full h-[40px] md:h-[45px]  rounded-md uppercase">
+            Get Them Both
+          </button>
         </div>
-        <div className='flex flex-col items-center'>
-            <h3 className='mb-4'>Disney Bundle Trio Basic</h3>
-            <Image src='/logo-bundle.png' alt='logo-bundle' width={150} height={200} />
-            <o className='mt-4 ml-4 text-white text-[16px] text-center font-semibold'>All with ads, for <br />  $14.99/month.</o>
-            <button className='text-black bg-[#00ED82] w-full h-[40px] rounded-sm uppercase mt-6'>Get All Three</button>
+        <div className="flex flex-col md:w-64 justify-center md:max-w-64 lg:w-[300px] lg:h-[300px] items-center">
+          <h3 className="mb-4">Disney Bundle Trio Basic</h3>
+          <Image
+            src="/logo-bundle.png"
+            alt="logo-bundle"
+            width={180}
+            height={300}
+          />
+          <o className="mt-4 ml-4 text-white text-[16px] md:text-[25px] text-center font-normal">
+            All with ads, for <br /> $14.99/month.
+          </o>
+          <button className="text-black bg-[#00ED82] w-full h-[40px] md:h-[45px] rounded-md uppercase mt-4">
+            Get All Three
+          </button>
+          
         </div>
-
       </div>
-   
-
+      <div className="text-white text-center text-sm underline font-light  ">
+        <p className="mt-6 mb-6 text-[14px]">Terms apply</p>
+        <p className="text-[16px]">Sign up for Hulu Only</p>
+      </div>
+     
+      
     </main>
-  )
+  );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,16 +12,16 @@ const Navbar = () => {
     (buttonClick) ? setbuttonClick(false) : setbuttonClick(true);
   }
   return (
-    <div className="flex  justify-between items-center  top-0 left-0 w-full z-50 px-6 py-5">
+    <div className="flex  justify-between items-center  top-0 left-0 w-full z-50  px-20 ">
       <div className="flex">
         <Image src={"/logo.png"} width={80} height={20} />
       </div>
       <div>
         <button className="text-white flex flex-col pt-4 items-end" onClick={handleClick}>
-          <div className="flex justify-center w-[45px] h-[35px] bg-[#3B3B3B] rounded-md md:hidden">
-            <RxHamburgerMenu className="h-4 w-4 my-auto cursor-pointer md:hidden" />
+          <div className="flex justify-center w-[45px] h-[35px] bg-[#3B3B3B] rounded-md lg:hidden">
+            <RxHamburgerMenu className="h-4 w-4 my-auto cursor-pointer lg:hidden" />
           </div>
-          <div className="mt-[15px]">
+          <div className="mt-[15px]  text-gray-300 text-md hidden lg:block">
             {buttonClick? (<button className="text-white" onClick={handleClick}>
              <div className="flex flex-col justify-around items-center w-[205px] h-[90px] border rounded-md font-inter uppercase ">
               <div className="">
@@ -34,8 +34,9 @@ const Navbar = () => {
               Get all three
               </div>
               </div>
-            </button>) : null}
+            </button>) : 'LOG IN'}
           </div>
+          
           
         </button>
       </div>
